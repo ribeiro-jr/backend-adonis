99ca91cdacccbcc86a0b45b0a74aaffa3a18fabc
Vanilson Ribeiro <ribeirovanilson85@gmail.com>
2022-11-30 15:51:10 +0100

    move generate slug to an helper class

---
+++ app/Helpers/Helper.ts
@@ -0,0 +1,10 @@
+export default class Helper {
+  public static slugify(text: string): string {
+    return text
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '')
+  }
+}
