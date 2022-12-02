ebf6a9f80914255b3fa7d3802c53650cdcd39634
Vanilson Ribeiro <ribeirovanilson85@gmail.com>
2022-12-02 09:09:41 +0100

    refactor upload service, ensure return error if throws

--- app/Services/UploadService.ts
+++ app/Services/UploadService.ts
@@ -5,17 +5,24 @@ import { MultipartFileContract } from '@ioc:Adonis/Core/BodyParser'
 export class UploadService {
   constructor() {}
 
-  public static async applicationStorage(file: MultipartFileContract, path: string) {
+  public static async applicationStorage(
+    file: MultipartFileContract,
+    path: string
+  ): Promise<{ name: string | null; error: object | null }> {
     try {
+      if (!file.isValid) {
+        return { name: null, error: file.errors }
+      }
+
       const filename = `${cuid()}.${file.extname}`
 
       await file.move(Application.tmpPath(path), {
         name: filename,
       })
 
-      return filename
+      return { name: filename, error: null }
     } catch (error) {
-      return null
+      return { name: null, error }
     }
   }
 }
